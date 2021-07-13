3897d0d34949670af3b03a8c087afe33f60edf36
Torres <dan03torres@gmail.com>
2021-07-13 20:41:51 -0300

    Update Program.cs

--- BancoBufunfaGUI/Program.cs
+++ BancoBufunfaGUI/Program.cs
@@ -22,29 +22,6 @@ namespace BancoBufunfaGUI
             ContaCorrente contaCorrente1 = new ContaCorrente(0);
             ContaPoupanca contaPoupanca1 = new ContaPoupanca(0);
 
-            /*
-            Console.Write("Insira o nome do titular da conta corrente: ");
-            contaCorrente1.SetTitular(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetTitular());
-            Console.Write("Insira o cpf do titular da conta corrente (ex: 000.000.000-00): ");
-            contaCorrente1.SetCpf(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetCpf());
-            Console.Write("Insira a profissão do titular da conta corrente: ");
-            contaCorrente1.SetProfissao(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetProfissao());
-
-
-            Console.Write("Insira o nome do titular da conta corrente: ");
-            contaCorrente1.SetTitular(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetTitular());
-            Console.Write("Insira o cpf do titular da conta corrente (ex: 000.000.000-00): ");
-            contaCorrente1.SetCpf(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetCpf());
-            Console.Write("Insira a profissão do titular da conta corrente: ");
-            contaCorrente1.SetProfissao(Console.ReadLine());
-            Console.WriteLine(contaCorrente1.GetProfissao());
-            */
-
             contaPoupanca1.SetSaldoDeposito(100);
             Console.WriteLine($"O rendimento da poupança deste mês foi de: {contaPoupanca1.GetRendimento()}");
 
